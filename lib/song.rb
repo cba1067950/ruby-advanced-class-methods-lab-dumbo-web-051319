9db7ae99349532be8bb51b2032a9126a6cda6b9a
Joe Jamsky <cba1067950@yahoo.com>
2019-05-17 16:00:30 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -27,7 +27,6 @@ class Song
   def self.create_by_name(name)
     song = Song.new_by_name(name) #or self.new_by_name(name)
     song.save                     #not Song.save << song
-    binding.pry
     song                          #Song.save overwrites array?
   end 
   
@@ -39,7 +38,7 @@ class Song
         return song_obj
       end 
     end 
-    #binding.pry 
+    binding.pry 
     false
   end 
   
